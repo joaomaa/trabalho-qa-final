d34a5afb18ad79b85fcf3f371b1b43a2a8ccc5d9
unknown <joaoantoniomaa@gmail.com>
2026-05-04 19:05:11 -0300

    feat: pipeline CI com GitHub Actions para API e Web

--- web_tests/conftest.py
+++ web_tests/conftest.py
@@ -7,6 +7,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=options
